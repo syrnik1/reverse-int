c88eb5d3b40e55bcbc540a77e0a0424848d9745a
syrnik1 <95712566+syrnik1@users.noreply.github.com>
2022-01-23 02:11:47 +0300

    update 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    let newArr = new Array();
+    let result = 0;
+    const str = String(Math.abs(n));                    // перевод числа в строку
+    const oldArr = Array.from(String(str), Number);          // перевод строки в числовой массив
+    
+    for (let i = 0; i < oldArr.length; i++) {
+        newArr[i] = oldArr[oldArr.length - 1 - i];
+    }
+
+    for (let i = 0; i < newArr.length; i++) {
+        result += newArr[i] * 10 ** (newArr.length - 1 - i);
+    }
+
+
+    return result;
 }
